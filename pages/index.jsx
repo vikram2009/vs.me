12a3e2c2988f7cc0068f3799b0fb880597efc59d
vikram2009 <vikramksm@hotmail.com>
2022-09-29 08:45:47 +0530

    meta update

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,25 +21,28 @@ export default function Home() {
         <meta
           data-react-helmet="true"
           name="description"
-          content="Combine your drives and clouds into one database that you can organize and explore from any device. Designed for creators, hoarders and the painfully disorganized."
+          content="13 year old full stack developer"
         />
         <meta
           data-react-helmet="true"
           property="og:image"
-          content="https://raw.githubusercontent.com/spacedriveapp/.github/main/profile/spacedrive_icon.png"
+          content="https://www.vikramsrinivas.me/icon.jpg"
         />
         <meta
           data-react-helmet="true"
           name="keywords"
-          content="files,file manager,spacedrive,file explorer,vdfs,distributed filesystem,cas,content addressable storage,virtual filesystem,photos app, video organizer,video encoder,tags,tag based filesystem"
+          content="files,file manager,codezzzzzzzzz,file explorer,vdfs,distributed filesystem,cas,content addressable storage,virtual filesystem,photos app, video organizer,video encoder,tags,tag based filesystem , vikram srinivas , vs , toxic , coder , programmer , vikramsrinivas , 13 year old  full stack developer , 13 year old software developer , toxic coder ,_toxic_coder , vikram2009 , vikrams , vsrinivas , rust , c , c++ , cpp , html , css , radix-ui , next-ui , 13 yeard old , 13 y/o , blog , blogger , vlog , vlogger , coder 24/7 , golang , tech , nerd"
         />
         <meta
           data-react-helmet="true"
           name="author"
-          content="Spacedrive Technology Inc."
+          content="Vikram Srinivas"
         />
-      </Head>
+        <link href="https://www.vikramsrinivas.me/icon.jpg" rel="canonical" />
+        <meta content="" property="og:image"/>
 
+
+</Head>
       <main>
         <Info />
         <About />
