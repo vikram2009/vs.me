10c051a77882520d5ad72350584ea3d76fb98e27
Vikram Srinivas <vikramksm@hotmail.com>
2022-12-06 21:37:27 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,32 @@ function MyApp({ Component, pageProps }) {
 <NextUIProvider theme={myDarkTheme}>
   
     <Component {...pageProps} />
+
+ <meta
+          data-react-helmet="true"
+          name="description"
+          content="13 year old full stack developer"
+        />
+        <meta
+          data-react-helmet="true"
+          property="og:image"
+          content="https://www.vikramsrinivas.me/icon.jpg"
+        />
+        <meta
+          data-react-helmet="true"
+          name="keywords"
+          content="files,file manager,codezzzzzzzzz,file explorer,vdfs,distributed filesystem,cas,content addressable storage,virtual filesystem,photos app, video organizer,video encoder,tags,tag based filesystem , vikram srinivas , vs , toxic , coder , programmer , vikramsrinivas , 13 year old  full stack developer , 13 year old software developer , toxic coder ,_toxic_coder , vikram2009 , vikrams , vsrinivas , rust , c , c++ , cpp , html , css , radix-ui , next-ui , 13 yeard old , 13 y/o , blog , blogger , vlog , vlogger , coder 24/7 , golang , tech , nerd"
+        />
+        <meta
+          data-react-helmet="true"
+          name="author"
+          content="Vikram Srinivas"
+        />
+        <link href="https://www.vikramsrinivas.me/icon.jpg" rel="canonical" />
+        <meta content="" property="og:image" />
 </NextUIProvider>
+
+
     </>)
     
   
